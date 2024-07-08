0b4bd2d6a34d55d7f604ce52aa4bf01e7997c17e
Sergiy Karatsuba <sergey.karatsuba@gmail.com>
2024-07-08 15:57:03 +0300

    add team id in config

--- config/config.ts
+++ config/config.ts
@@ -7,7 +7,7 @@ export const config = {
       create_folder: 'space/90151393719/folder',
     },
     space:{
-      create_space: 'team/{your_team_id}/space',
+      create_space: 'team/9015704772/space',
       invalid_create_space: 'team/ttt/space'
     }
   }
